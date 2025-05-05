c77a6cf73776486ae45a019f42e3a795f72625b7
TakumiMatsuda0630 <mattunari0630@icloud.com>
2025-05-06 06:55:17 +0900

    update: デフォルトレイアウトの設定を追加

--- application/resources/js/app.js
+++ application/resources/js/app.js
@@ -5,16 +5,19 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import DefaultLayout from './layout/DefaultLayout.vue'
 
 const vuetify = createVuetify({
     components,
     directives,
-  })
+})
 
 createInertiaApp({
     resolve: name => {
         const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
-        return pages[`./Pages/${name}.vue`]
+        let page = pages[`./Pages/${name}.vue`]
+        page.default.layout = page.default.layout || DefaultLayout
+        return page
     },
     setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
